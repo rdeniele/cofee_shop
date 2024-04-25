39ab1b460bf29ff3275f2a009b63229ce6a25f85
rdeniele <rondenielep13@gmail.com>
2024-04-25 17:11:22 +0800

    1st commit: added migrations

---
+++ database/migrations/2024_04_25_085433_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->string('status');
+            $table->timestamp('order_date');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
